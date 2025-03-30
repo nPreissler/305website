ac9725b2a218791caaae9b62387ed041b95d563d
nPreissler <nestorpreissler@gmail.com>
2025-03-30 00:19:39 -0300

    feat: prices, orders working and copy 'PIX'

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,38 @@
-// // alert("Leia com atenção antes de enviar suas informações")
-
+// alert("Leia com atenção antes de enviar suas informações")
 
     document.getElementById("pedidoForm").onsubmit = function(e) {
         e.preventDefault();
 
         var checkboxes = document.querySelectorAll("input[type=checkbox]:checked");
-        var selectedOptions = Array.from(checkboxes).map(cb => cb.value).join(", ");
+        var selectedOptions = Array.from(checkboxes).map(cb => cb.value);
+        var totalSelecionados = selectedOptions.length;
 
-        if (selectedOptions.length === 0) {
+        if (totalSelecionados === 0) {
             alert("Por favor, selecione pelo menos um complemento!");
             return;
         }
 
+        var preco = (totalSelecionados >= 3) ? "a pagar 13,50" : "a pagar 10,50";
+
+        var mensagemFinal = selectedOptions.join(", ") + " - " + preco;
+
         var fieldID = "entry.943827343"; 
 
         var formUrl = "https://docs.google.com/forms/d/e/1FAIpQLScE5qGCLpDvGLfDfzNULI7kHcUq9M2E_jDudA3l7-5C5O1R5w/viewform?" 
-                    + encodeURIComponent(fieldID) + "=" + encodeURIComponent(selectedOptions);
+                    + encodeURIComponent(fieldID) + "=" + encodeURIComponent(mensagemFinal);
 
         window.location.href = formUrl;
     };
+
+    function copiarTexto() {
+        var texto = document.getElementById("copyText").innerText;
+        
+        var tempInput = document.createElement("textarea");
+        tempInput.value = texto;
+        document.body.appendChild(tempInput);
+        tempInput.select();
+        document.execCommand("copy");
+        document.body.removeChild(tempInput);
+
+        alert("Texto copiado: " + texto);
+    }
